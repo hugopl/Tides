35c4dc006d2502ccd8b7e6bd12f17d8359dd9473
Hugo Parente Lima <hugo.pl@gmail.com>
2011-12-30 18:58:58 -0300

    Update version number.

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
     QDeclarativeView viewer;
     viewer.setResizeMode(QDeclarativeView::SizeRootObjectToView);
     viewer.rootContext()->setContextProperty("locations", QVariant::fromValue(model.locations()));
-    viewer.rootContext()->setContextProperty("TIDES_VERSION", "1.0.0");
+    viewer.rootContext()->setContextProperty("TIDES_VERSION", "1.0.1");
     viewer.rootContext()->setContextProperty("tides", &model);
     viewer.setSource(QUrl("qrc:/main.qml"));
     viewer.showFullScreen();
